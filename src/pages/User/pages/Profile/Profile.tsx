c9387a254fd7ce0dd9cebee02035a6ae5365ba36
KDEV <lekietpro123@gmail.com>
2023-08-18 21:37:45 +0700

    code UI for page Profile

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,5 +1,89 @@
-import React from 'react'
+import Input from 'src/components/Input'
 
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>
+          Hồ sơ của tôi
+        </h1>
+        <div className='mt-1 text-sm text-gray-700'>
+          Quản lý thông tin hồ sơ để bảo mật tài khoản
+        </div>
+      </div>
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          <div className='flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
+              Email
+            </div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='pt-3 text-gray-700'>k****@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
+              Tên
+            </div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
+              Số điện thoại
+            </div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
+              Địa chỉ
+            </div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
+              Ngày sinh
+            </div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='flex justify-between'>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  Ngày
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  Tháng
+                </select>
+                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  Năm
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className='md:border-l-gray.200 flex justify-center md:w-72 md:border-l'>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                src='https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+                alt=''
+                className='h-full w-full rounded-full object-cover'
+              />
+            </div>
+            <input type='file' accept='.jpg, .jpeg, .png' className='hidden' />
+            <button className='flex h-10 items-center justify-center rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3 text-xs text-gray-400'>
+              <div>Dung lượng ảnh tối đa 1MB</div>
+              <div>Định dạng: .JPEG, .PNG</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
