e54c95c62ff7d32d4df0e5bf6dd1936231fbd7ee
KDEV <lekietpro123@gmail.com>
2023-08-18 18:25:31 +0700

    handle error when token expired

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useContext, useEffect } from 'react'
 import useElementsRoutes from './useElementsRoutes'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { localStorageEventTarget } from './utils/auth'
+import { AppContext } from './contexts/app.context'
 
 function App() {
   const routeElements = useElementsRoutes()
+  const { reset } = useContext(AppContext)
+
+  useEffect(() => {
+    localStorageEventTarget.addEventListener('clearLS', reset)
+
+    return () => {
+      localStorageEventTarget.removeEventListener('clearLS', reset)
+    }
+  }, [reset])
+
   return (
     <div>
       <ToastContainer />
