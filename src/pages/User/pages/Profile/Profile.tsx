b5e4999f4136ea433b98a5577e28341d19532b79
KDEV <lekietpro123@gmail.com>
2023-08-22 00:10:58 +0700

    show data profile on form part 2

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import userAPi from 'src/apis/user.api'
@@ -7,6 +7,7 @@ import Button from 'src/components/Button'
 import Input from 'src/components/Input'
 import InputNumber from 'src/components/InputNumber'
 import { UserSchema, userSchema } from 'src/utils/rules'
+import DateSelect from '../../components/DateSelect'
 
 type FormData = Pick<
   UserSchema,
@@ -22,6 +23,14 @@ const profileSchema = userSchema.pick([
 ])
 
 export default function Profile() {
+  const { data: profileData } = useQuery({
+    queryKey: ['profile'],
+    queryFn: userAPi.getProfile
+  })
+
+  const profile = profileData?.data.data
+  const updateProfileMutation = useMutation(userAPi.updateProfile)
+
   const {
     register,
     formState: { errors },
@@ -41,13 +50,6 @@ export default function Profile() {
     resolver: yupResolver(profileSchema)
   })
 
-  const { data: profileData } = useQuery({
-    queryKey: ['profile'],
-    queryFn: userAPi.getProfile
-  })
-
-  const profile = profileData?.data.data
-
   useEffect(() => {
     if (profile) {
       setValue('name', profile.name || '')
@@ -63,6 +65,13 @@ export default function Profile() {
     }
   }, [profile, setValue])
 
+  const onSubmit = handleSubmit(async (data) => {
+    await updateProfileMutation.mutateAsync({})
+  })
+
+  const value = watch()
+  console.log(value, errors)
+
   return (
     <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
@@ -73,7 +82,10 @@ export default function Profile() {
           Quản lý thông tin hồ sơ để bảo mật tài khoản
         </div>
       </div>
-      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+      <form
+        onSubmit={onSubmit}
+        className='mt-8 flex flex-col-reverse md:flex-row md:items-start'
+      >
         <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
           <div className='flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
@@ -128,24 +140,19 @@ export default function Profile() {
               />
             </div>
           </div>
-          <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>
-              Ngày sinh
-            </div>
-            <div className='sm:w-[80%] sm:pl-5'>
-              <div className='flex justify-between'>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  Ngày
-                </select>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  Tháng
-                </select>
-                <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                  Năm
-                </select>
-              </div>
-            </div>
-          </div>
+
+          <Controller
+            control={control}
+            name='date_of_birth'
+            render={({ field }) => (
+              <DateSelect
+                errorMessage={errors.date_of_birth?.message}
+                value={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+
           <div className='mt-8 flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'></div>
             <div className='sm:w-[80%] sm:pl-5'>
