d4931732a0adea89f7c0cdee6326eac08f8a50b9
ASUS <lekietpro123@gmail.com>
2026-03-10 17:31:00 +0700

    Handle error simply message with axios interceptor.

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
+import { HttpStatusCode } from 'src/constants/httpStatusCode.enum'
 
 // Tạo ra một http để dễ gọi API
 class Http {
@@ -11,9 +13,23 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function onFulfilled(response) {
+        return response
+      },
+      function onRejected(error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
 const http = new Http().instance
-
 export default http
